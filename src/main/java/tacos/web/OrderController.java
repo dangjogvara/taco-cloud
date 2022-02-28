b34357aba42dab44b7ac41d9b99f97d1a38acb78
Dan Poulsen <dangjogvara@gmail.com>
2022-02-28 20:57:47 +0000

    postmapping submit order

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,12 +1,14 @@
 package tacos.web;
 
-
 import lombok.extern.slf4j.Slf4j;
+import tacos.TacoOrder;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
-
+import org.springframework.web.bind.support.SessionStatus;
 
 @Slf4j
 @Controller
@@ -14,8 +16,16 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 @SessionAttributes("tacoOrder")
 public class OrderController {
 
-    @GetMapping("/current")
-    public String orderForm() {
-        return "orderForm";
-    }
+	@GetMapping("/current")
+	public String orderForm() {
+		return "orderForm";
+	}
+
+	@PostMapping
+	public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+		log.info("Order submittet: {}", order);
+		sessionStatus.setComplete();
+
+		return "redirect:/";
+	}
 }
