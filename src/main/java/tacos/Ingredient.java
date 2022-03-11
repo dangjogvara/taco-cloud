7d3f5e98ac896048776f7c84ede95fa6cd772d4f
Dan Poulsen <dangjogvara@gmail.com>
2022-03-11 22:48:37 +0000

    add jpa notation

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,22 +1,24 @@
 package tacos;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
 @Data
-@Table
+@Entity
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@NoArgsConstructor
 public class Ingredient {
 
     @Id
-    private final String id;
-    private final String name;
-    private final Type type;
+    private String id;
+    private String name;
+    private Type type;
+
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
