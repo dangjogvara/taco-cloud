4b40f77ef53bd53e6b1668fdbf3a6a227310d516
Dan Poulsen <dangjogvara@gmail.com>
2022-03-11 22:51:52 +0000

    removed unused import

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -3,7 +3,6 @@ package tacos;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
